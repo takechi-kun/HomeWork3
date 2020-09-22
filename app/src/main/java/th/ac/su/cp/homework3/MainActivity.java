ebe07cd00dcabf76bc7e162a57543dc1300648dc
Songwarin Wiratchakun <chicubpom1234@gmail.com>
2020-09-22 16:44:50 +0700

    homework3

--- app/src/main/java/th/ac/su/cp/homework3/MainActivity.java
+++ app/src/main/java/th/ac/su/cp/homework3/MainActivity.java
@@ -15,7 +15,6 @@ import android.widget.TextView;
 import th.ac.su.cp.homework3.model.Item;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,61 +27,58 @@ public class MainActivity extends AppCompatActivity {
         rv.setLayoutManager(lm); //config layout manager ให้กับ RecyclerView
         rv.setAdapter(adapter);//config adapter ให้กับ RecyclerView
     }
+}
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    Item[]items = {
+            new Item(R.drawable.thailand, "THAILAND", "ประเทศไทย"),
+            new Item(R.drawable.south_korea, "SOUTH KOREA","ประเทศเกาหลีใต้"),
+            new Item(R.drawable.united_states, "UNITED STATES","สหรัฐอเมริกา"),
+            new Item(R.drawable.united_kingdom, "UNITED KINGDOM","สหราชอาณาจักร"),
+            new Item(R.drawable.italy, "ITALY","ประเทศอิตาลี"),
+            new Item(R.drawable.japan, "JAPAN","ประเทศญี่ปุ่น"),
+            new Item(R.drawable.mexico, "MEXICO","ประเทศเม็กซิโก"),
+            new Item(R.drawable.spain, "SPAIN","ประเทศสเปน"),
+            new Item(R.drawable.taiwan, "TAIWAN","ประเทศไต้หวัน"),
+            new Item(R.drawable.finland, "FINLAND","ประเทศฟินแลนด์"),
+    };
 
-
-    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-        Item[]items = {
-                new Item(R.drawable.thailand, "THAILAND"),
-                new Item(R.drawable.south_korea, "SOUTH KOREA"),
-                new Item(R.drawable.united_states, "UNITED STATES"),
-                new Item(R.drawable.united_kingdom, "UNITED KINGDOM"),
-                new Item(R.drawable.italy, "ITALY"),
-                new Item(R.drawable.japan, "JAPAN"),
-                new Item(R.drawable.mexico, "MEXICO"),
-                new Item(R.drawable.spain, "SPAIN"),
-                new Item(R.drawable.taiwan, "TAIWAN"),
-                new Item(R.drawable.finland, "FINLAND"),
-        };
-
-        public MyAdapter() {
-        }
-
-        //ctrl+o
-        @NonNull
-        @Override
-        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item, parent, false);
-            MyViewHolder vh = new MyViewHolder(v);
-            return vh;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
-            holder.imageView.setImageResource(items[position].imageResId);
-            holder.wordTextView.setText(items[position].word);
-        }
-
-        @Override
-        public int getItemCount() {
-            return items.length;
-        }
-
-        class MyViewHolder extends RecyclerView.ViewHolder{
-            ImageView imageView;
-            TextView wordTextView;
-
-            public MyViewHolder(@NonNull View itemView) {
-                super(itemView);
-                imageView = itemView.findViewById(R.id.image_view);
-                wordTextView = itemView.findViewById(R.id.item_text_view);
-            }
-        }
-
+    public MyAdapter() {
     }
 
+    //ctrl+o
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
 
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        holder.imageView.setImageResource(items[position].imageResId);
+        holder.wordTextView.setText(items[position].word);
+        holder.translateTextView.setText(items[position].translate);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.length;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView wordTextView;
+        TextView translateTextView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image_view);
+            wordTextView = itemView.findViewById(R.id.item_text_view);
+            translateTextView = itemView.findViewById(R.id.translate_text_view);
+        }
+    }
 
 }
--- app/src/main/java/th/ac/su/cp/homework3/model/Item.java
+++ app/src/main/java/th/ac/su/cp/homework3/model/Item.java
@@ -3,9 +3,10 @@ package th.ac.su.cp.homework3.model;
 public class Item {
     public final int imageResId;
     public final String word;
-
-    public Item(int imageResId, String word) {
+    public final String translate;
+    public Item(int imageResId, String word, String translate) {
         this.imageResId = imageResId;
         this.word = word;
+        this.translate = translate;
     }
 }
